d830776f1a7061dfcc8e80dbddc09fcd73ac163a
Rukshani <arukshani@gmail.com>
2018-08-18 17:10:56 +0530

    Add plain java nio server/client code

--- client/src/main/java/EchoClientHandler.java
+++ client/src/main/java/EchoClientHandler.java
@@ -10,14 +10,19 @@ import io.netty.util.CharsetUtil;
 @Sharable
 public class EchoClientHandler
         extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private int count ;
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!" + System.currentTimeMillis(),
+        System.out.println("~~~~~~Client channelActive()");
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Client sends this : " + System.currentTimeMillis(),
                 CharsetUtil.UTF_8));
+
     }
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
+        System.out.println("~~~~~~Client channelRead0()" + " - " + + ++count);
         System.out.println(
                 "Client received: " + in.toString(CharsetUtil.UTF_8) + "  " + System.currentTimeMillis());
     }
